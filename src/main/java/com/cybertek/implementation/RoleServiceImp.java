302ce4ce3025bdaed7419aeadad3a01705ef807d
kalguns <kalguns@gmail.com>
2021-01-31 18:48:37 +0100

    Ticketing Project - ORM

--- src/main/java/com/cybertek/implementation/RoleServiceImp.java
+++ src/main/java/com/cybertek/implementation/RoleServiceImp.java
@@ -2,33 +2,38 @@ package com.cybertek.implementation;
 
 import com.cybertek.dto.RoleDTO;
 import com.cybertek.entity.Role;
+import com.cybertek.mapper.RoleMapper;
 import com.cybertek.repository.RoleRepository;
 import com.cybertek.service.RoleService;
+import com.fasterxml.jackson.core.json.async.NonBlockingJsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class RoleServiceImp implements RoleService {
 
     @Autowired
     private RoleRepository roleRepository;
+    private RoleMapper roleMapper;
 
-    @Override
-    public List<RoleDTO> listAllRoles() {
-
-        List<Role> list = roleRepository.findAll();
-
-        //convert to DTO and return it - why we need mapper
-
-
-
-        return null;
+    public RoleServiceImp(RoleRepository roleRepository, RoleMapper roleMapper) {
+        this.roleRepository = roleRepository;
+        this.roleMapper = roleMapper;
     }
 
     @Override
-    public RoleDTO findById(long id) {
-        return null;
+    public List<RoleDTO> listAllRoles() {
+        List<Role> list = roleRepository.findAll();
+        //convert to DTO and return it - why we need mapper
+        return list.stream().map(obj -> {return roleMapper.convertToDto(obj);}).collect(Collectors.toList());
+    }
+
+    @Override
+    public RoleDTO findById(Long id) {
+        Role role = roleRepository.findById(id).get();
+        return roleMapper.convertToDto(role);
     }
 }
